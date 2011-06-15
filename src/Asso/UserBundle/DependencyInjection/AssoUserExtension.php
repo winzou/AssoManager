b45729baaa7873cb740ab07a8813483b257cac92
winzou <alexandre.bacco@gmail.com>
2011-06-15 19:30:50 +0800

    [UserBundle] Customize user provider, fix #6

---
+++ src/Asso/UserBundle/DependencyInjection/AssoUserExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Asso\UserBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class AssoUserExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('orm.xml');
+    }
+
+    public function getAlias()
+    {
+        return 'asso_user';
+    }
+}
