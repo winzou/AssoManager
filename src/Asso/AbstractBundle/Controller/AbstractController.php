257c4cf172df88174be7b0d9dd233bd457768586
winzou <alexandre.bacco@gmail.com>
2011-07-20 11:59:26 +0800

    [Abstract] typo

--- src/Asso/AbstractBundle/Controller/AbstractController.php
+++ src/Asso/AbstractBundle/Controller/AbstractController.php
@@ -42,34 +42,6 @@ abstract class AbstractController extends Controller
         return parent::render ( $view , $parameters , $response );
     }
 
-    /*public function createForm($type, $data = null, array $options = array())
-    {
-        if( $type instanceof FormTypeInterface AND $data === null )
-        {
-
-        }
-        AND ! isset($options['data_class'])
-        AND $data === null )
-        {
-            if( ! isset($options['data_class']) )
-            {
-                // get the namespace without the last Form/XxxType
-                $namespace = explode('\\', get_class($type), -2);
-
-                // trying to build the right namespace, like BaseNamespace\Entity\Xxx
-                $options['data_class'] = implode('\\', $namespace) . '\\Entity\\' . $type->getName();
-            }
-
-            // instanciate the entity
-            $data = new $options['data_class'];
-
-            // we log this action as it could lead to a confusing behavior
-            $this->get('logger')->debug('[Asso] Automaticaly guess the data_class to "'.$options['data_class'].'" when creating a Form around "'.get_class($type).'"');
-        }
-
-        return parent::createForm($type, $data, $options);
-    }*/
-
     /**
      * Do this controller need AssoSelector listener look for an Asso?
      * @return false
@@ -107,7 +79,7 @@ abstract class AbstractController extends Controller
         return $this->get('asso_am.asso_selector')->getAsso();
     }
 
-/**
+	/**
      * Return the current Asso id
      * @return int
      */
