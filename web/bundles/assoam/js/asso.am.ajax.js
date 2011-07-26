f74b6e4b3ec69a9bc77faf82b30d9e78e8ed8e1e
winzou <alexandre.bacco@gmail.com>
2011-07-26 19:55:56 +0800

    Revert "[web] add bundle resources (ugly till assetic is as well)"
    This reverts commit 7b6fff2efe1ef2a9edfbb4dcd7c5a3a6e484389f.

---
+++ web/bundles/assoam/js/asso.am.ajax.js
@@ -0,0 +1,103 @@
+
+$(document).ready(function() {
+    $(document).ajaxError(function(event, jqXHR) {
+        var message;
+        
+        try {
+            var response = JSON.parse(jqXHR.responseText);
+            message = response.message ? response.message : 'Sf2 said: ' + response[0].message;
+        }
+        catch(err) {
+        	/** @todo why doesn't it find my title? */
+        	title = $(jqXHR.responseText).find('title').text();
+        	
+        	if(title) {
+        		message = title;
+        	}
+        	else {
+        		message = jqXHR.responseText;
+        	}
+        }
+        
+        if(!message)
+        {
+            if(403 === jqXHR.status) {
+                message = 'Acces denied, please login again.';
+            }
+            else if(404 == jqXHR.status) {
+                message = 'Page not found.';
+            }
+            else if(500 == jqXHR.status) {
+                message = 'Server-side error.';
+            }
+            else {
+                message = 'Unknown error!';
+            }
+        }
+        
+        alert(message);
+    });
+});
+
+function asso_ajx_callback(trigger, url, callback) {
+	//$("#result").html(ajax_load);
+	$.ajax({
+		type:     'POST',
+		url:      url,
+		dataType: 'json',
+		success:  function(json) {
+			if(!json.code) {
+				alert( json[0].message ? json[0].message : 'Something went terribly wrong, server side.');
+			}
+			else {
+				callback(trigger);
+			}
+		    //$("#result").html(result);
+		 }
+	});
+}
+
+function asso_ajx_confirm($this, url, callback) {
+	$this.fastConfirm({
+		position:     "right",
+		questionText: "Are you sure?",
+		unique:       true,
+		onProceed:    function(trigger) {
+			asso_ajx_callback(trigger, url, callback);
+			$(trigger).fastConfirm('close');
+		}
+	});
+}
+
+
+function asso_ajx_form_enable($this, url, postCallback, preCallback) {
+	var options = {
+		beforeSubmit: preCallback,  // pre-submit callback 
+		success:      postCallback,  // post-submit callback
+		dataType:     'json',
+		url:          url ? url : $this.attr('action')
+	};
+
+	//attach handler to form's submit event 
+	$this.submit(function() { 
+	    // submit the form 
+	    $(this).ajaxSubmit(options); 
+	    // return false to prevent normal browser submit and page navigation 
+	    return false; 
+	});
+}
+
+function asso_ajx_form_submit($this, url, postCallback, preCallback) {
+	var options = {
+			beforeSubmit: preCallback,  // pre-submit callback 
+			success:      postCallback,  // post-submit callback
+			dataType:     'json',
+			url:          url ? url : $this.attr('action')
+		};
+	
+	$(this).ajaxSubmit(options); 
+}
+
+
+
+
