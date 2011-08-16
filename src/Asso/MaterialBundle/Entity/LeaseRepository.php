f22d63f3d26bee951b574fd9826e75d7200ceb2f
winzou <alexandre.bacco@gmail.com>
2011-08-16 14:31:37 +0800

    [Material] Refactor Manager to Repository

---
+++ src/Asso/MaterialBundle/Entity/LeaseRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Asso\BookBundle\Entity;
+
+use Asso\AbstractBundle\Manager\AbstractRepository;
+use Asso\BookBundle\Entity\Account;
+
+/**
+ * LeaseRepository
+ * @author winzou
+ */
+class LeaseRepository extends AbstractRepository
+{
+}
---
+++ src/Asso/MaterialBundle/Entity/MaterialRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Asso\BookBundle\Entity;
+
+use Asso\AbstractBundle\Manager\AbstractRepository;
+use Asso\MaterialBundle\Entity\Material;
+
+use Doctrine\ORM\Query;
+
+/**
+ * MaterialRepository
+ * @author winzou
+ */
+class MaterialRepository extends AbstractRepository
+{
+}
